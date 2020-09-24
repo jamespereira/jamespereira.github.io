975da31ba663ba18a9dd57231417b617d4ab435c
James Pereira <james.pereira90@gmail.com>
2020-09-24 16:44:07 +1000

    add my-games to portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -14,6 +14,14 @@ class Portfolio extends Component {
                     </a>
                     <p>Simple <a href="https://jamespereira.github.io/todo-list/">Todo List</a> list using react, bulma css and firebase for realtime database integration.</p>
                 </div>
+
+                <div>
+                    <h3>My Games</h3>
+                    <a href="https://i.imgur.com/FvywWTp.png" target="_blank" rel="noopener noreferrer" className="imageLink">
+                        <img height="200px;" alt="Todo List web app" src="https://i.imgur.com/FvywWTp.png" />
+                    </a>
+                    <p>A sorted shelf of <a href="https://jamespereira.github.io/my-games/">my games</a>. SPA built off learnings from Udacity react course, including ES6, Public API and Firebase for realtime database integration.</p>
+                </div>
             </div>
         );
     }
